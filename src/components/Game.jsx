2f8256843fa95397d1b3cc8729c0387042434c4c
irakligudiashvili <54039911+irakligudiashvili@users.noreply.github.com>
2024-10-21 21:58:33 +0400

    added algorithm to pick dog breeds and render them

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -9,10 +9,30 @@ function Game(){
         {breed: 'Poodle', src: 'src/assets/poodle.jpeg', isClicked: false},
         {breed: 'Bernard', src: 'src/assets/bernard.png', isClicked: false},
         {breed: 'Shih Tzu', src: 'src/assets/shihtzu.png', isClicked: false},
+        {breed: 'Pug', src: 'src/assets/pug.jpg', isClicked: false},
     ]
 
+    function selectBreeds(breeds){
+        const unclickedBreeds = breeds.filter(breed => !breed.isClicked);
+
+        if(unclickedBreeds.length === 0) {
+            // Player wins
+        }
+
+        const randomUnclickedBreed = unclickedBreeds[Math.floor(Math.random() * unclickedBreeds.length)];
+
+        const remainingBreeds = breeds.filter(breed => breed !== randomUnclickedBreed);
+
+        const shuffledRemainingBreeds = remainingBreeds.sort(() => 0.5 - Math.random());
+
+        const currentSelection = [randomUnclickedBreed, ...shuffledRemainingBreeds.slice(0, 5)];
+
+        return currentSelection;
+    }
+
+    const currentSelection = selectBreeds(breeds);
     
-    const breedCards = breeds.map(breed => 
+    const breedCards = currentSelection.map(breed => 
         <div className='card' key={breed.breed}>
             <img className='cardImg' src={breed.src}></img>
             <p className='cardText'>{breed.breed}</p>
