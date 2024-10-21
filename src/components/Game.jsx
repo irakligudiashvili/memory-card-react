2b9c43c33d83eef4f84cecc29a534263978648a0
irakligudiashvili <54039911+irakligudiashvili@users.noreply.github.com>
2024-10-21 22:23:38 +0400

    added more breeds

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -10,6 +10,21 @@ function Game(){
         {breed: 'Bernard', src: 'src/assets/bernard.png', isClicked: false},
         {breed: 'Shih Tzu', src: 'src/assets/shihtzu.png', isClicked: false},
         {breed: 'Pug', src: 'src/assets/pug.jpg', isClicked: false},
+        {breed: 'Boxer', src: 'src/assets/boxer.jpg', isClicked: false},
+        {breed: 'Beagle', src: 'src/assets/beagle.png', isClicked: false},
+        {breed: 'Husky', src: 'src/assets/husky.webp', isClicked: false},
+        {breed: 'Spitz', src: 'src/assets/spitz.webp', isClicked: false},
+        {breed: 'Maltese', src: 'src/assets/maltese.webp', isClicked: false},
+        {breed: 'Collie', src: 'src/assets/collie.webp', isClicked: false},
+        {breed: 'Dalmatian', src: 'src/assets/dalmatian.webp', isClicked: false},
+        {breed: 'Otterhound', src: 'src/assets/otterhound.webp', isClicked: false},
+        {breed: 'Whippet', src: 'src/assets/whippet.webp', isClicked: false},
+        {breed: 'Maltipoo', src: 'src/assets/maltipoo.webp', isClicked: false},
+        {breed: 'Lowchen', src: 'src/assets/lowchen.webp', isClicked: false},
+        {breed: 'Chihuahua', src: 'src/assets/chihuahua.webp', isClicked: false},
+        {breed: 'Cavapoo', src: 'src/assets/cavapoo.webp', isClicked: false},
+        {breed: 'Briad', src: 'src/assets/briad.webp', isClicked: false},
+        {breed: 'Bolognese', src: 'src/assets/bolognese.webp', isClicked: false},
     ]
 
     function selectBreeds(breeds){
